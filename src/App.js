65f47aae1b8498c0c47a6db8a4246d5c0cb53e9c
Harry Chen <hc287@duke.edu>
2022-07-11 23:23:16 -0400

    Add user context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
+import { UserContext } from './contexts/UserContext';
 import './App.css';
 import { NavLink, Routes, Route } from 'react-router-dom';
 import NavBar from './components/navigation/NavBar'
@@ -6,14 +7,36 @@ import NavBar from './components/navigation/NavBar'
 import Home from './components/Home';
 import Store from './components/Store';
 
+import {onAuthStateChanged, getAuth} from 'firebase/auth';
+
 function App() {
+  const auth = getAuth();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (u) => {
+      if (u) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/firebase.User
+          // ...
+          setUser(u);
+      } else {
+          // User is signed out
+          // ...
+          setUser(null);
+      }
+    });
+  }, [])
+
   return (
     <main>
-      <NavBar />
-      <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/store" element={<Store />} />
-      </Routes>
+      <UserContext.Provider value={user}>
+        <NavBar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/store" element={<Store />} />
+        </Routes>
+      </UserContext.Provider>
     </main>
   );
 }
--- src/components/navigation/NavContent.js
+++ src/components/navigation/NavContent.js
@@ -1,32 +1,34 @@
-import React, {useState} from 'react';
-import {handleLogin} from "./login";
-import {onAuthStateChanged, GoogleAuthProvider, getAuth} from 'firebase/auth';
-import {app} from '../../firebase.js';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { UserContext } from '../../contexts/UserContext';
+import { handleLogin } from "./login";
+import { app } from '../../firebase.js';
 import './NavBar.css';
 
-const provider = new GoogleAuthProvider();
-const auth = getAuth();
-
 function NavContent() {
-    const [user, setUser] = useState(auth.currentUser? auth.currentUser : null);
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            // User is signed in, see docs for a list of available properties
-            // https://firebase.google.com/docs/reference/js/firebase.User
-            // ...
-            setUser(user);
-        } else {
-            // User is signed out
-            // ...
-            setUser(null);
-        }
-    });
-    
-    return (<div id="NavContent">
-        <li id="navItem"><a id="navButton" href="../"><span id="navSpan">HOME</span></a></li>
-        <li id="navItem"><a id="navButton" href="../store"><span id="navSpan">STORE</span></a></li>
-        <li id="navItem"><div id="navButton" onClick={(e) => handleLogin(e)}><span id="navSpan">{user? "SIGN OUT" : "SIGN IN"}</span></div></li>
-    </div>)
+    return (
+        <UserContext.Consumer>
+            {(user) => (
+                <div id="NavContent">
+                    <li id="navItem">
+                        <Link id="navButton" to="../">
+                            <span id="navSpan">HOME</span>
+                        </Link>
+                    </li>
+                    <li id="navItem">
+                        <Link id="navButton" to="../store">
+                            <span id="navSpan">STORE</span>
+                        </Link>
+                    </li>
+                    <li id="navItem">
+                        <div id="navButton" onClick={(e) => handleLogin(e)}>
+                            <span id="navSpan">{user? "SIGN OUT" : "SIGN IN"}</span>
+                        </div>
+                    </li>
+                </div>
+            )}
+        </UserContext.Consumer>
+    )
 }
 
 export default NavContent;
