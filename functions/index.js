9e1db2b8114b0e4e65640c3a9a29c1fd73be905a
Alexander Yoshida <ayoshida@unc.edu>
2022-08-07 14:05:59 -0400

    added changeUserBalance and getUidFromEmail

--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,18 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 const { auth } = require("firebase-admin");
 const { user } = require("firebase-functions/v1/auth");
+const { set } = require("firebase/database");
 admin.initializeApp();
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
+
+const getRole = async (context) => {
+    let userRef = admin.database().ref("users/" + context.auth.uid + "/role");
+    let snap = await userRef.once("value");
+    let userData = snap.val();
+    return userData;
+}
 
 // performs a transaction
 exports.transaction = functions.https.onCall(async (data, context) => {
@@ -31,14 +39,12 @@ exports.newUser = functions.https.onCall(async (data, context) => {
         if (userData == null) {
             console.log("CREATE NEW USER");
             userRef.update({
-                balance: 0,
+                balance: {
+                    amount: 0
+                },
                 name: context.auth.token.name || "",
                 email: context.auth.token.email || "",
                 role: "user",
-                cart: {
-                    next_id: 0,
-                    items: {}
-                },
                 inventory: {
                     next_id: 0,
                 }
@@ -49,7 +55,7 @@ exports.newUser = functions.https.onCall(async (data, context) => {
     }
 });
 
-exports.addToCart = functions.https.onCall(async (data, context) => {
+exports.addToCart = functions.https.onCall(async (data={id: "", amount: 0}, context) => {
     if (!context.auth) {
         // Throwing an HttpsError so that the client gets the error details.
         throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
@@ -60,15 +66,14 @@ exports.addToCart = functions.https.onCall(async (data, context) => {
     let userData = snap.val();
     if (userData == null)
         return;
-    userRef.child("cart").transaction(function(cart) {
-        let id = cart.next_id;
-        cart[id] = data.id;
-        cart.next_id += 1;
-        return cart;
-    });
+    let itemRef = userRef.child("cart").child(data.id);
+    itemRef.transaction(function(value) {
+        return value + data.amount;
+    })
 });
 
-exports.getUidFromEmail = functions.https.onCall(async (data, context) => {
+exports.getUidFromEmail = functions.https.onCall(async (email, context) => {
+    const validRoles = ["accountant", "admin", "service"];
     if (!context.auth) {
         // Throwing an HttpsError so that the client gets the error details.
         throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
@@ -76,22 +81,61 @@ exports.getUidFromEmail = functions.https.onCall(async (data, context) => {
     }
 
     // verify role of caller
-    let userRef = admin.database().ref("users/" + context.auth.uid + "/role");
-    let snap = await userRef.once("value");
-    let userData = snap.val();
-    if (userData == null)
-        return null;
+    let role = await getRole(context);
+    if (role == null || !validRoles.includes(role))
+        return {error: "role not valid"};
     
     // search for email
     let allUsersRef = admin.database().ref("users");
     snap = await allUsersRef.once("value");
     let allUsersData = snap.val();
     if (allUsersData == null)
-        return null;
+        return {error: "failed to retrieve users data"};
+    console.log(allUsersData);
     for (let uid in allUsersData) {
-        if (allUsersData[uid].email === data.email) {
-            return uid;
+        if (allUsersData[uid].email === email) {
+            return {value: uid};
         }
     }
-    return null;
-})
+    return {value: null};
+});
+
+exports.changeUserBalance = functions.https.onCall(async (data={uid: "", amount: 0, description: "", placeholder_account: false}, context) => {
+    if (!context.auth) {
+        // Throwing an HttpsError so that the client gets the error details.
+        throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
+        'while authenticated.');
+    }
+
+    // verify accountant role
+    let role = await getRole(context);
+    if (role != "accountant")
+        return {error: "role not valid"}
+
+    // add balance change
+    let userRef = placeholder_account? admin.database().ref("pacc/" + uid) : admin.database().ref("users/" + uid);
+    await userRef.child("balance").transaction(function(value) {
+        if (value == null)
+            value = {};
+        if (value.records == null)
+            value.records = {};
+        
+        // get time
+        let time = Date.now();
+        while (time in value.records) {
+            await new Promise(r => setTimeout(r, 100));
+            time = Date.now();
+        }
+
+        // add new balance change
+        value.records[time] = {amount: amount, description: description};
+
+        // update balance
+        let balance = 0;
+        for (let change in value.records) {
+            balance += change.amount;
+        }
+        value.amount = balance;
+        return value;
+    })
+});
