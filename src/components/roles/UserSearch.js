fdbe82c799bfa8dc82ffa86c203161485d32188a
Alexander Yoshida <ayoshida@unc.edu>
2022-08-08 23:06:48 -0400

    minor visual changes

--- src/components/roles/UserSearch.js
+++ src/components/roles/UserSearch.js
@@ -5,9 +5,9 @@ function UserSearch({onSubmit, disabled}) {
 
     return (
         <form onSubmit={(event) => {event.preventDefault(); onSubmit(email);}} autocomplete="off">
-            <label for="user_email">Email</label>
-            <input type="text" id="user_email" onChange={(e) => {setEmail(e.target.value)}} disabled={disabled}></input>
-            <input type="submit" value="Submit" disabled={disabled}></input>
+            <label for="user_email">Email: </label>
+            <input type="text" id="user_email" placeholder="enter user email here" onChange={(e) => {setEmail(e.target.value)}} disabled={disabled}></input>
+            <input type="submit" value="Fetch" disabled={disabled}></input>
         </form>
     )
 }
